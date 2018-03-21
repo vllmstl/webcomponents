21e80ac2d854b25f405ad5b6481d4a2cbd6547ff
Karthik Vallamsetla <karthik.vallamsetla@macys.com>
2018-03-21 15:13:07 -0700

    webcomponents - dynamic attribute

--- spinner.js
+++ spinner.js
@@ -1,6 +1,7 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <div>Hello world</div>
+    <div id="percentValue"></div>
 `;
 class Spinner extends HTMLElement {
     constructor() {
@@ -8,14 +9,26 @@ class Spinner extends HTMLElement {
         this.attachShadow({mode: 'open'});
         this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
+    set percent(val) {
+        this.setAttribute("percent", val);
+    }
     connectCallback() {
 
     }
     disconnectedCallback() {
 
     }
+    static get observedAttributes() {
+        return ['percent'];
+    }
     attributeChangedCallback(name, oldVal, newVal) {
-
+        console.log("val changed", name, oldVal, newVal);
+        switch (name) {
+            case 'percent':
+                this.shadowRoot.querySelector('#percentValue').innerHTML = newVal;
+                break;
+            default:
+        }
     }
 }
 
